6829e500293108cd1eee373d3c2cbb34ff2e00ff
whybigsz <ricardo.ferreira.2222@gmail.com>
2022-11-10 21:06:19 +0000

    teste

--- src/bin/Servidor/Servidor1.java
+++ src/bin/Servidor/Servidor1.java
@@ -1,9 +1,21 @@
 package bin.Servidor;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 
 public class Servidor1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MulticastSocket ms = new MulticastSocket(4004);
+
+
+        while (true){
+            byte[] recData = new byte[256];
+            DatagramPacket dp = new DatagramPacket(recData,recData.length);
+            ms.receive(dp);
+            String msg = new String(dp.getData(),0,dp.getLength());
+            System.out.println(msg);
+        }
+
     }
 }
